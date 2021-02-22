5a38074d8d363a2e08e98b271615a791555eec9d
Stanley <2001stany@gmail.com>
2021-02-22 23:43:06 +0300

    add:testing script

--- main_app/api.py
+++ main_app/api.py
@@ -1,7 +1,31 @@
 import requests
 
-url = 'http://127.0.0.1:8000/test/'
-headers = {'Authorization': 'Token f31ad0ec5a17baa1c419d5ab8d424e5b3a408087'}
-r = requests.get(url, headers=headers)
+'''
+A separate script to test the API. Just incase tools like Postman fails !
+
+
+'''
+
+# GET method - 'test/ endpoint'
+
+# url = 'http://127.0.0.1:8000/test/'
+# headers = {'Authorization': 'Token f31ad0ec5a17baa1c419d5ab8d424e5b3a408087'}
+# r = requests.get(url, headers=headers)
+# print(r.status_code)
+# print(r.content)
+
+
+
+# post method
+
+url = 'http://127.0.0.1:8000/'
+
+data = {
+    'email-receiver': '2001stany@gmail.com, doccasheby@gmail.com',
+    'email-subject': 'send from api',
+    'email-body': 'hello'
+}
+
+r = requests.post(url, data=data)
 print(r.status_code)
-print(r.content)
+print(r.reason)
