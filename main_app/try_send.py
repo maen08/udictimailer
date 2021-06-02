4b31624947ba9072234c77a8dbd08fdb143f8dbf
maen08 <2001stany@gmail.com>
2021-06-02 13:47:38 +0300

    fix: template

--- main_app/try_send.py
+++ main_app/try_send.py
@@ -6,7 +6,7 @@ headers = {'Authorization': 'Token 0d328fbb8812b3552e8053109d0adcd26c1cd55b'}
 data = {
     'email-receiver': '2001stany@gmail.com',
     'email-subject': 'Sent from API',
-    'email-body': 'Hello UdictiMailer',
+    'email-body': 'Hello UdictiMailer hhhhhhhhhhhhhhhhhhhhhhhhkdddddddddddddddddfffffffffkdfdkfdkhfkd',
 }
 
 r = requests.post(url, data=data, headers=headers)
