0af080b395af23a94b3eee030bb89873d84ffc3b
maen08 <2001stany@gmail.com>
2021-06-02 12:43:50 +0300

    add: dependencies

---
+++ main_app/try_send.py
@@ -0,0 +1,15 @@
+import requests
+
+url = 'http://udictimailer.herokuapp.com/send/'
+headers = {'Authorization': 'Token 0d328fbb8812b3552e8053109d0adcd26c1cd55b'}
+
+data = {
+    'email-receiver': '2001stany@gmail.com',
+    'email-subject': 'Sent from API',
+    'email-body': 'Hello UdictiMailer',
+}
+
+r = requests.post(url, data=data, headers=headers)
+print(r.status_code)
+print(r.reason)
+print(r.content)
