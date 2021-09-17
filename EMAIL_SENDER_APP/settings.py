c0bafd217527522dd0d62754c739862d9c9d7d33
jackkweyunga <jackkweyunga@gmail.com>
2021-09-17 17:38:27 +0300

    set app pssword

--- EMAIL_SENDER_APP/settings.py
+++ EMAIL_SENDER_APP/settings.py
@@ -137,7 +137,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 EMAIL_BACKEND = 'des.backends.ConfiguredEmailBackend'
 
 
-
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
