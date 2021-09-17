7d46d4666c6a3bc918750b909a6e1e688dc70cc1
jackkweyunga <jackkweyunga@gmail.com>
2021-09-17 17:33:30 +0300

    ...

--- EMAIL_SENDER_APP/settings.py
+++ EMAIL_SENDER_APP/settings.py
@@ -126,14 +126,15 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'udictii@gmail.com'  # add to env. variable
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = True
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = 'udictii@gmail.com'  # add to env. variable
+# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+# EMAIL_USE_TLS = True
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 
+EMAIL_BACKEND = 'des.backends.ConfiguredEmailBackend'
 
 
 
--- main_app/try_send.py
+++ main_app/try_send.py
@@ -1,10 +1,10 @@
 import requests
 
-url = 'http://udictimailer.herokuapp.com/send/'
-headers = {'Authorization': 'Token 0d328fbb8812b3552e8053109d0adcd26c1cd55b'}
+url = 'http://127.0.0.1:8000/send/'
+headers = {'Authorization': 'Token 51dbcaf6ee8afcb191dcf853a3a3c638fedb0edf'}
 
 data = {
-    'email-receiver': '2001stany@gmail.com',
+    'email-receiver': 'jackkweyunga@gmail.com',
     'email-subject': 'Sent from API',
     'email-body': 'Hello UdictiMailer hhhhhhhhhhhhhhhhhhhhhhhhkdddddddddddddddddfffffffffkdfdkfdkhfkd',
 }
@@ -12,4 +12,4 @@ data = {
 r = requests.post(url, data=data, headers=headers)
 print(r.status_code)
 print(r.reason)
-print(r.content)
+# print(r.content)
